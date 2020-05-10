bafb5956e8d9a7aa71cb0a463e0edf0055d4c2c0
pkro <pkrombach@web.de>
2020-05-10 09:42:50 +0200

    example custom services

--- api/product/services/product.js
+++ api/product/services/product.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  find(params, populate) {
+    console.log(params);
+    // you could email something here for example; doesn't seem to be the right place for filtering by custom parameters
+
+    // if (params._titles_only) {
+    //   return strapi
+    //     .query("product")
+    //     .find(params, populate)
+    //     .map((product) => product.name);
+    // }
+
+    return strapi.query("product").find(params, populate);
+  },
+};
