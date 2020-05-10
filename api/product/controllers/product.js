51f7a5acbac13eb236aaa32508a29a79b6627244
pkro <pkrombach@web.de>
2020-05-10 09:32:03 +0200

    example for customized controller

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,19 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    const products = await strapi.services.product.find(ctx.query);
+
+    return products.map((product) => ({
+      ...product,
+      description: product.description
+        ? product.description
+        : "No description available",
+    }));
+  },
+};
